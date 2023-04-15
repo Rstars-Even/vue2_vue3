8d58f53a828ccb8a9a1b7fa499db04395e9b9d58
Rstars-Even <473753137@qq.com>
2023-04-15 23:59:27 +0800

    用户管理页面编写。

--- vue_project/admin/src/view/User.vue
+++ vue_project/admin/src/view/User.vue
@@ -1,12 +1,87 @@
 <template>
-    <h1>我是 user 页面。。。</h1>
+    <div class="manage">
+        <el-dialog
+            title="提示"
+            :visible.sync="dialogVisible"
+            width="50%">
+            <!-- 用户从表单信息。。 -->
+            <el-form ref="form" :rules="rules" :model="form" :inline="true" label-width="80px">
+                <el-form-item label="姓名" prop="name">
+                    <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="年龄" prop="age">
+                    <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
+                </el-form-item>
+                <el-form-item label="性别" prop="sex">
+                    <el-select v-model="form.sex" placeholder="请选择">
+                        <el-option label="男" value="1"></el-option>
+                        <el-option label="女" value="0"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="出生日期" prop="birth">
+                    <el-date-picker
+                        v-model="form.birth"
+                        type="date"
+                        placeholder="选择日期">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item label="地址" prop="addr">
+                    <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
+                </el-form-item>
+            </el-form>
+
+
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="submit">确 定</el-button>
+            </span>
+        </el-dialog>
+        <div class="manage-header">
+            <el-button type="primary" @click="dialogVisible = true">
+                + 新增
+            </el-button>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
-
+                dialogVisible: false,
+                form: {
+                    name: '',
+                    age: '',
+                    sex: '',
+                    birth: '',
+                    addr: '',
+                },
+                rules: {
+                    name: [
+                        { required: true, message: '请输入姓名' },
+                    ],
+                    age: [
+                        { required: true, message: '请输入年龄' },
+                    ],
+                    sex: [
+                        { required: true, message: '请选择性别' },
+                    ],
+                    birth: [
+                        { required: true, message: '请选择出生日期' },
+                    ],
+                    addr: [
+                        { required: true, message: '请输入地址' },
+                    ],
+                }
+            }
+        },
+        methods: {
+            submit() {
+                this.$refs.form.validate((valid) => {
+                    if (valid) {        //填写的表单是否检测通过。。
+                        
+                    }
+                })
             }
         }
     }
