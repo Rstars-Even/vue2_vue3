7fff5b8ae178fbd8e243572e1f1144c201295854
Rstars-Even <473753137@qq.com>
2023-06-20 18:07:54 +0800

    头条项目。。。

---
+++ vue_project2_demo/demo-toutiao/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <van-tabbar route>
+    <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+    <van-tabbar-item icon="user-o" to="/about">我的</van-tabbar-item>
+  </van-tabbar>
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
