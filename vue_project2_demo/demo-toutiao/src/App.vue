11ec66e380e1177ea540c88db9a02664583aef35
Rstars-Even <473753137@qq.com>
2023-06-21 18:24:03 +0800

    头条项目首页。。

--- vue_project2_demo/demo-toutiao/src/App.vue
+++ vue_project2_demo/demo-toutiao/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
+    <router-view></router-view>
     <van-tabbar route>
-    <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
-    <van-tabbar-item icon="user-o" to="/about">我的</van-tabbar-item>
-  </van-tabbar>
+      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="user-o" to="/about">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
--- vue_project2_demo/demo-toutiao/src/views/Home.vue
+++ vue_project2_demo/demo-toutiao/src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='home-container'>
+    <van-nav-bar title='怒发冲冠' fixed/>
+    <ArtItem
+      v-for='item in artList'
+      :key='item.art_id'
+      :title='item.title'
+      :name='item.aut_name'
+      :count="item.comm_count"
+      :time="item.pubdate"
+      :cover="item.cover"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { getArticleListAPI } from '@/api/articleAPI.js'
+import ArtItem from '@/components/ArtItem.vue'
 export default {
   name: 'Home',
+  data() {
+    return {
+      page: 1,
+      limt: 10,
+      artList: []
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    async getList() {
+      const { data: res } = await getArticleListAPI(this.page, this.limt)
+      console.log('--------res-----', res)
+      this.artList = res
+    }
+  },
   components: {
-    HelloWorld
+    ArtItem
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .home-container {
+    padding: 46px 0 50px 0;
+    .van-nav-bar {
+      background-color: #007bff;
+    }
+    /deep/ .van-nav-bar__title {
+      color: white;
+    }
+  }
+</style>
